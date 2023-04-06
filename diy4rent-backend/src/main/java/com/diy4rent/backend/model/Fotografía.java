1114bd98f1a1a9265eef66212353ce97b4928496
abrilom <129775176+abrilom@users.noreply.github.com>
2023-04-06 18:32:57 +0200

    Update Fotografía.java

--- diy4rent-backend/src/main/java/com/diy4rent/backend/model/Fotografía.java
+++ diy4rent-backend/src/main/java/com/diy4rent/backend/model/Fotografía.java
@@ -1,1 +1,18 @@
+package com.diy4rent.backend.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "fotografíass")
+
+public class Fotografías {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  
+}
