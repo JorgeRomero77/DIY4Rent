7068b5c3a5740253e31eb911c9a7da04ea208ae5
abrilom <129775176+abrilom@users.noreply.github.com>
2023-04-07 17:50:32 +0200

    Update Intervalo.java

--- diy4rent-backend/src/main/java/com/diy4rent/backend/model/Intervalo.java
+++ diy4rent-backend/src/main/java/com/diy4rent/backend/model/Intervalo.java
@@ -36,5 +36,19 @@ public class Intervalo {
   public void setId(int id) {
 		this.id = id;
 	}
+	
+  public String getInicio() {
+		return inicio;
+	}
+	
+  public void setInicio(String inicio) {
+		this.inicio = inicio;
+	}
+  public String getFin() {
+		return fin;
+	}
+  public void setFin(String fin) {
+		this.fin = fin;
+	}
   
 }
