8599640154db61d4925d14c27a7846fb0e7556af
abrilom <129775176+abrilom@users.noreply.github.com>
2023-04-07 17:36:59 +0200

    Update Fotografía.java

--- diy4rent-backend/src/main/java/com/diy4rent/backend/model/Fotografía.java
+++ diy4rent-backend/src/main/java/com/diy4rent/backend/model/Fotografía.java
@@ -15,4 +15,14 @@ public class Fotografías {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
   
+
+
+  public int getId() {
+		return id;
+	  }
+
+  public void setId(int id) {
+		this.id = id;
+	}
+  
 }
