50739978c66a16f1b1a05b3d13a0d52d9d76a39d
abrilom <129775176+abrilom@users.noreply.github.com>
2023-04-07 17:44:16 +0200

    Update Intervalo.java

--- diy4rent-backend/src/main/java/com/diy4rent/backend/model/Intervalo.java
+++ diy4rent-backend/src/main/java/com/diy4rent/backend/model/Intervalo.java
@@ -1,1 +1,30 @@
+package com.diy4rent.backend.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "intervalos")
+
+public class Intervalos {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  private String inicio;
+  private String fin;
+  
+
+
+  public int getId() {
+		return id;
+	  }
+
+  public void setId(int id) {
+		this.id = id;
+	}
+  
+}
